9f5926c52d0f792892960ee5faf65280779bc594
Anna Karp <ana-karp@users.noreply.github.com>
2020-10-26 20:54:18 +0300

    init: start x-press-publishing

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+
+const app = express();
+
