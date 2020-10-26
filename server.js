d760d3388f4241993b83533146949ea05b99bebe
Anna Karp <ana-karp@users.noreply.github.com>
2020-10-26 23:44:24 +0300

    feat: setup server

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const morgan = require('morgan');
 const errorhandler = require('errorhandler');
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
+});
+
+module.exports = app;
 
